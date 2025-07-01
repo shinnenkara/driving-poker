3e49f962b5d3faaf72c54d99c1f035742ac95cf2
Oleksii Kharchenko <Doom9936.1999@gmail.com>
2025-07-01 14:58:54 +0200

    Initial

---
+++ app/page.tsx
@@ -0,0 +1,32 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <div>
+          <Button asChild>
+            <Link
+              className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
+              href={"/cards"}
+            >
+              Cards
+            </Link>
+          </Button>
+        </div>
+      </main>
+      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+        <Button asChild>
+          <Link
+            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
+            href={"https://github.com/shinnenkara"}
+            target={"_blank"}
+          >
+            GitHub
+          </Link>
+        </Button>
+      </footer>
+    </div>
+  );
+}
